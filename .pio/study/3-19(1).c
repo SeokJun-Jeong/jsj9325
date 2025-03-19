d797657c87c53bdb532ca4e1fb1f18c034abf916
jsj9325 <seokjun9325@naver.com>
2025-03-19 11:27:28 +0900

    3-191

--- .pio/study/3-19(1).c
+++ .pio/study/3-19(1).c
@@ -10,21 +10,27 @@ int main()
     DDRC = 0x0F;
     PORTE = _BV(2) ; //풀업 설정
 
+    Icdinit();
+    IcdClear();
     while (1)
     {
-        // if (PINE)
-        // {
-        //     switchFlag = PINE >> 2;
-        // }
-        // if (!(switchFlag & ))
-        // {
-        //     PORTC ^=0x01; // 1번 비트 반전
+        if (PINE)
+        {
+            switchFlag = PINE >> 2;
+        }
+        if (!(switchFlag & ))
+        {
+            PORTC ^=0x01; // 1번 비트 반전
             
-        // }
-        PORTC ^= 0x01;
-        _delay_ms(500);
+        }
+        // PORTC ^= 0x01;
+        // _delay_ms(500);
+
         lcdGotoXY(0,0);
-        lcdBuffer
+        lcdBuffer[17] ="";
+        sprintf(IcdBuffer, "%d", switchFlag * _BV(PC0));
+        //strcat(IcdBuffer, "0" + PORTC);
+        IcdPrint(IcdBuffer);
     }
     
     return 0;
