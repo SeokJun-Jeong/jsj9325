17ed29f51719156476b7f05f555161517168351b
jsj9325 <seokjun9325@naver.com>
2025-03-20 09:38:07 +0900

    23

--- src/main.c
+++ src/main.c
@@ -1,59 +1,37 @@
-//인터럽트로 FND를 점멸
-//INT5  phase 1 > phase 2 or phase 2 > phase 1
-//phase 1: FND가 빠르게 상승하는 코드 delay 100
-//phase 2: FND를 순간 멈추게 함
+// 채터링 error유발
+
 
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #include <util/delay.h>
-
-volatile uint8_t timeStop = 0; 
-volatile char txData = 0;
-
-uint8_t getch(void)
-{
-    uint8_t data;
-    while ((UCSR0A & 0x80) == 0) // 문자버퍼에 있으면 루프 탈출
-        ;
-     data = UDR0;
-    UCSR0A |= 0x80;
-    return data;
-    
-}
 
 int main()
 {
-    uint8_t numbers[]={0x3F, 0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x27,0x7F,0x67};
-    uint8_t rxData;
-    DDRA = 0xFF;
-    
-    UCSR0A = 0x00;
-    UCSR0B = 0x18; //0b00011000 Rx, Tx enable
-    UCSR0C = 0x16; //0b00010110 비동기 , no Parity, 1stop bit
+    unsigned char switchFlag= 0;
+    DDRC = 0x0F;
+    PORTE = _BV(2) ; //풀업 설정
 
-    UBRR0H = 0x00;
-    UBRR0L = 0x07; //115200 bps
-
+    Icdinit();
+    IcdClear();
     while (1)
-    {   
-        rxData = getch();  
-        if ((rxData >= 0x30) && (rxData <= 0x39))
+    {
+        if (PINE)
         {
-            PORTA = numbers[rxData - 0x30];
+            switchFlag = PINE >> 2;
         }
-        
+        if (!(switchFlag & ))
+        {
+            PORTC ^=0x01; // 1번 비트 반전
+            
+        }
+        // PORTC ^= 0x01;
+        // _delay_ms(500);
+
+        lcdGotoXY(0,0);
+        lcdBuffer[17] ="";
+        sprintf(IcdBuffer, "%d", switchFlag * _BV(PC0));
+        //strcat(IcdBuffer, "0" + PORTC);
+        IcdPrint(IcdBuffer);
     }
+    
     return 0;
-}
-
-
-
- 
-
-
-SIGNAL(INT5_vect)
-{
-    cli();
-    timeStop ^= 1;
-    sei();
 }
