dea4aa308d2fe9e3dd2d0c21fe007f8a000e132d
jsj9325 <seokjun9325@naver.com>
2025-03-25 15:30:07 +0900

    sadmf

--- lib/uart0/uart0.h
+++ lib/uart0/uart0.h
@@ -1,6 +1,6 @@
 #ifndef UART1_H_
 #define UART1_H_
-#include<stdio.h>
+#include <stdio.h>
 
 void uart0Init(void);
 void uart0Transmit(char data);
@@ -9,7 +9,6 @@ void uart0PrintString(char *str);
 void uart0Print1ByteNumber(unsigned char n);
 
 FILE OUTPUT = FDEV_SETUP_STREAM(uart0Transmit, NULL, _FDEV_SETUP_WRITE);
-FILE INPUT = FDEV_SETUP_STREAM( NULL,uart0Receive, _FDEV_SETUP_READ);
-
+FILE INPUT = FDEV_SETUP_STREAM(NULL, uart0Receive, _FDEV_SETUP_READ);
 
 #endif // UART1_h_
