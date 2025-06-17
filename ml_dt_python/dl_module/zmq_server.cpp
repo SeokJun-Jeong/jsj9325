2160d9f2a0f5ea3d02588b5e60b7251554e4c5bb
jsj9325 <seokjun9325@naver.com>
2025-06-17 13:49:04 +0900

    123

---
+++ ml_dt_python/dl_module/zmq_server.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <zmq.hpp>
+#include <string>
+
+using namespace std;
+using namespace zmq;
+
+int main()
+{
+    context_t context(1);
+    socklen_t responder(context, socket_type::rep);
+    const char *ipc_file_path="/tmp/zmq_server";
+    if (unlink(ipc_file_path) == -1 && errno != ENOENT)
+    {
+        cerr << "Error removing existing IPC file : " << strerror(errno) << endl;
+        return 1;
+    }
+    responder.bind("ipc://" + string(ipc_file_path));
+    cout << "서버가 IPC소켓에 바인딩 되었습니다" << ipc_file_path << endl;
+    while (true)
+    {
+        message_t request;
+        recv_result_t result = responder.recv(request, recv_flags::none);
+        if (result && result.value() > 0)
+        {
+            string received_msg = request.to_string();
+            cout << "받은 메세지: " << received_msg << endl;
+            string reply_msg = "메세지 수신됨" + received_msg; //echo메세지
+            responder.send(message_t(reply_msg), send_flags::none);
+        }
+        
+    }
+    
+    return 0;
+}
